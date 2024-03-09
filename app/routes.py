c5a3e4bd129e3864bc5503c754b8f009cd354383
soufiene-houssem <soufiene.houssem@courrier.uqam.ca>
2024-03-08 22:45:44 -0500

    Changement mineur du build

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,13 @@
-from flask import render_template, Blueprint
 from datetime import datetime
+from flask import render_template, Blueprint
+import pytz
+from sqlalchemy.orm import joinedload
 from models.model import Livre
 from models.model  import Categorie
-from sqlalchemy.orm import joinedload
 from configs.config import cache
-import pytz
 
 app_routes = Blueprint('app_routes', __name__)
 
-"""
-Afficher le temps actuel
-"""
 @app_routes.route('/')
 def index():
     montreal_tz = pytz.timezone('America/Toronto')
@@ -18,9 +15,6 @@ def index():
     message = "Biblio"
     return render_template('index.html', date=montreal_time, message=message)
 
-"""
-Récupérer les livres de la db
-"""
 @app_routes.route('/livres')
 @cache.cached(timeout=10)
 def get_livres():
@@ -33,17 +27,12 @@ def get_livres():
             joinedload(Livre.auteurs) 
         ).all()
         cache.set('livres', livres, timeout=50)
-        
     return render_template('livres.html', books=livres, categories=categories)
 
-"""
-Récupérer le contenu du cache
-"""
 @app_routes.route('/contenu-cache')
 def cache_content():
     cached_livres = cache.get('livres') 
     if cached_livres:
         return render_template('cache.html', cached_books=cached_livres)
-    else:
-        message = 'Aucun contenu dans le cache'
-        return render_template('cache.html', message=message)
+    message = 'Aucun contenu dans le cache'
+    return render_template('cache.html', message=message)
